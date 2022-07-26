c65810a3eadbd3250704971551cf80dc021f821e
Euan Macfarlane <euanmacfarlane@MacBook-Pro.lan>
2022-07-26 14:37:35 +0100

    vote button added to single article

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -6,6 +6,12 @@ import {useState, useEffect} from 'react'
 function SingleArticle () {
     const {article_id} = useParams();
     const [article, setArticle] = useState({});
+    const articleCopy = article
+    console.log(articleCopy,'<<<<Single Article')
+
+    function displayArticle() {
+
+    }
 
     useEffect(()=>{
         axios.get(`https://nc-news-example-1.herokuapp.com/api/articles/${article_id}`).then((res)=>{
@@ -19,7 +25,12 @@ function SingleArticle () {
             <h2>Title: {article.title}</h2>
             <h3>Topic: {article.topic}</h3>
             <h3>Author: {article.author}</h3>
-            <h3>Created: {article.created_at}</h3>
+            <p>{article.body}</p>
+            <h3>Vote: {article.votes}</h3><button onClick={(e)=>{
+                axios.patch(`https://nc-news-example-1.herokuapp.com/api/articles/${article_id}`,{votes: 1}).then(
+
+                )
+            }}>Vote</button>
         </div>
     )
 }
